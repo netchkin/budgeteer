92ca6aeae9114e518cc2318a2400ae862f9c42e3
netchkin <pavel.janecka@gmail.com>
2017-02-12 22:01:39 +0100

    add flowtype

--- src/Components/ExpenseList.js
+++ src/Components/ExpenseList.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React, {Component} from 'react';
 import ExpenseListItem from './ExpenseListItem';
 import {observer} from 'mobx-react';
--- src/Components/ExpenseListItem.js
+++ src/Components/ExpenseListItem.js
@@ -1,6 +1,8 @@
+// @flow
+
 import React from 'react';
 
-export default ({ expense, onDelete }) => {
+export default ({ expense, onDelete }: { expense: {title: string, id: string, value: number}, onDelete: {(x: string): void}}) => {
   return (
     <div>
       <div>{expense.title}<i onClick={onDelete(expense.id)}>X</i></div>
