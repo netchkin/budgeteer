16c1aa37eed8c3fe4912b6d349111811ee01f6f5
netchkin <pavel.janecka@gmail.com>
2017-02-13 07:26:00 +0100

    PropTypes for ExpenseListItem.js

--- src/Components/ExpenseListItem.js
+++ src/Components/ExpenseListItem.js
@@ -2,10 +2,21 @@
 
 import React from 'react';
 
-export default ({ expense, onDelete }: { expense: {title: string, id: string, value: number}, onDelete: {(): void}}) => {
+const ExpenseListItem = ({ expense, onDelete }: { expense: {title: string, id: string, value: number}, onDelete: {(): void}}) => {
   return (
     <div>
       <div>{expense.title}<i onClick={onDelete}>X</i></div>
       <span>value</span><span>{expense.value}</span>
     </div>);
 };
+
+ExpenseListItem.propTypes = {
+  expense: React.PropTypes.shape({
+    id: React.PropTypes.string,
+    value: React.PropTypes.string,
+    title: React.PropTypes.string
+  }).isRequired,
+  onDelete: React.PropTypes.func.isRequired
+};
+
+export default ExpenseListItem;
